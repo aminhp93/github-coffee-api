5c049d1cbc2c951080f5d6160a6d54d7ecb641e7
aminhp93 <minhpn.org.ec@gmail.com>
2024-03-30 16:38:05 +0700

    Update mui package, add display type

--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,30 @@
 
 // Import libraries
 import dynamic from "next/dynamic";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import {
   Box,
   Accordion,
   AccordionSummary,
   AccordionDetails,
+  ToggleButton,
+  ToggleButtonGroup,
+  styled,
 } from "@mui/material";
-import { ExpandMore } from "@mui/icons-material";
+import {
+  ExpandMore,
+  DataObject,
+  TableRows,
+  ShowChart,
+} from "@mui/icons-material";
 
 // Import local files
 import { getRequest } from "./utils";
 import { TOKEN, LIST_API } from "./constants";
-import Request from "../@core/components/request";
+import Request from "@/@core/components/request";
+import Chart from "@/@core/components/chart";
+import Table from "@/@core/components/table";
 import { useRequestStore } from "./store";
 
 const Response = dynamic(() => import("@/@core/components/response"), {
@@ -26,11 +36,14 @@ const TestResult = dynamic(() => import("@/@core/components/test-result"), {
   ssr: false,
 });
 
+type Display = "raw-api" | "table" | "chart";
+
 const Home = () => {
   const request = useRequestStore((s) => s.request);
   const response = useRequestStore((s) => s.response);
   const setResponse = useRequestStore((s) => s.setResponse);
   const allResponses = useRequestStore((s) => s.allResponses);
+  const [display, setDisplay] = useState<Display>("raw-api");
 
   useEffect(() => {
     if (!request) return;
@@ -58,15 +71,33 @@ const Home = () => {
     })();
   }, [request, setResponse]);
 
+  const handleChange = (
+    event: React.MouseEvent<HTMLElement>,
+    newAlignment: Display
+  ) => {
+    setDisplay(newAlignment);
+  };
+
+  const children = [
+    <ToggleButton value="raw-api" key="raw-api">
+      <DataObject />
+    </ToggleButton>,
+    <ToggleButton value="table" key="table">
+      <TableRows />
+    </ToggleButton>,
+    <ToggleButton value="chart" key="chart">
+      <ShowChart />
+    </ToggleButton>,
+  ];
+
+  const control = {
+    value: display,
+    onChange: handleChange,
+    exclusive: true,
+  };
+
   return (
-    <Box
-      sx={{
-        display: "flex",
-        gap: "20px",
-        padding: "20px",
-        height: "100vh",
-      }}
-    >
+    <StyledBoxRoot>
       <Box sx={{ flex: 1, minWidth: 0, overflow: "auto", height: "100%" }}>
         {LIST_API.map((item) => (
           <Accordion key={item.id}>
@@ -89,21 +120,37 @@ const Home = () => {
       </Box>
       <Box sx={{ flex: 1, overflow: "auto" }}>
         <Box sx={{ height: "500px", overflow: "auto" }}>
-          <Response
-            requestObject={{
-              token: TOKEN,
-              url: request?.url,
-            }}
-            responseData={response}
-          />
+          <ToggleButtonGroup size="small" {...control} aria-label="Small sizes">
+            {children}
+          </ToggleButtonGroup>
+          {display === "raw-api" && (
+            <Response
+              requestObject={{
+                token: TOKEN,
+                url: request?.url,
+              }}
+              responseData={response}
+            />
+          )}
+
+          {display === "table" && <Table />}
+
+          {display === "chart" && <Chart />}
         </Box>
         <hr />
         <Box>
           <TestResult />
         </Box>
       </Box>
-    </Box>
+    </StyledBoxRoot>
   );
 };
+
+const StyledBoxRoot = styled(Box)({
+  display: "flex",
+  gap: "20px",
+  padding: "20px",
+  height: "100vh",
+});
 
 export default Home;
