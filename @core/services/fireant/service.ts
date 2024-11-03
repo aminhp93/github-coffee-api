2c032bb34095a2f41f9f4b1c659ca90cb41d41ad
aminhp93 <minhpn.org.ec@gmail.com>
2024-11-03 09:19:36 +0700

    [86cwz2035] fix fireant request

--- @core/services/fireant/service.ts
+++ @core/services/fireant/service.ts
@@ -10,12 +10,21 @@ import {
 import dayjs from "dayjs";
 
 import {
-  HistoricalPriceResponse,
-  FinancialReportsResponse,
   FundamentalResponse,
   NewsResponse,
   PostsResponse,
   WatchlistsResponse,
+  ProfileResponse,
+  RrgResponse,
+  HistoricalPriceResponse,
+  FinancialReportsResponse,
+  TimescaleMarksResponse,
+  OfficersResponse,
+  SubsidiariesResponse,
+  HoldersResponse,
+  DividendsResponse,
+  EventsResponse,
+  FinancialIndicatorsResponse,
 } from "./schema";
 
 export const baseURL = "https://restv2.fireant.vn";
@@ -35,40 +44,6 @@ httpFireantService.interceptors.response.use((res) => {
 });
 
 const FireantService = {
-  historicalPrice: (
-    symbol: string,
-    startDate: string = "2021-06-23",
-    endDate: string = dayjs().format("YYYY-MM-DD"), // "2024-06-23
-    offset: number = DEFAULT_OFFSET,
-    limit: number = DEFAULT_LIMIT
-  ): Promise<HistoricalPriceResponse> =>
-    httpFireantService({
-      url: FireantUrls.historicalPrice(
-        symbol,
-        startDate,
-        endDate,
-        offset,
-        limit
-      ),
-    }),
-  financialReports: (
-    symbol: string,
-    type: FinancialReportsType = "IS",
-    period: Period = "Q",
-    compact: boolean = true,
-    offset: number = DEFAULT_OFFSET,
-    limit: number = 5
-  ): Promise<FinancialReportsResponse> =>
-    httpFireantService({
-      url: FireantUrls.financialReports(
-        symbol,
-        type,
-        period,
-        compact,
-        offset,
-        limit
-      ),
-    }),
   fundamental: (symbol: string): Promise<FundamentalResponse> =>
     httpFireantService({
       url: FireantUrls.fundamental(symbol),
@@ -93,6 +68,87 @@ const FireantService = {
   watchlists: (): Promise<WatchlistsResponse> => {
     return httpFireantService({
       url: FireantUrls.watchlists(),
+    });
+  },
+  profile: (): Promise<ProfileResponse> => {
+    return httpFireantService({
+      url: FireantUrls.profile(),
+    });
+  },
+  rrg: (): Promise<RrgResponse> => {
+    return httpFireantService({
+      url: FireantUrls.rrg(),
+    });
+  },
+
+  financialReports: (
+    symbol: string,
+    type: FinancialReportsType = "IS",
+    period: Period = "Q",
+    compact: boolean = true,
+    offset: number = DEFAULT_OFFSET,
+    limit: number = 5
+  ): Promise<FinancialReportsResponse> =>
+    httpFireantService({
+      url: FireantUrls.financialReports(
+        symbol,
+        type,
+        period,
+        compact,
+        offset,
+        limit
+      ),
+    }),
+
+  historicalPrice: (
+    symbol: string,
+    startDate: string = "2021-06-23",
+    endDate: string = dayjs().format("YYYY-MM-DD"), // "2024-06-23
+    offset: number = DEFAULT_OFFSET,
+    limit: number = DEFAULT_LIMIT
+  ): Promise<HistoricalPriceResponse> =>
+    httpFireantService({
+      url: FireantUrls.historicalPrice(
+        symbol,
+        startDate,
+        endDate,
+        offset,
+        limit
+      ),
+    }),
+  timescaleMarks: (): Promise<TimescaleMarksResponse> => {
+    return httpFireantService({
+      url: FireantUrls.timescaleMarks(),
+    });
+  },
+  officers: (): Promise<OfficersResponse> => {
+    return httpFireantService({
+      url: FireantUrls.officers(),
+    });
+  },
+  subsidiaries: (): Promise<SubsidiariesResponse> => {
+    return httpFireantService({
+      url: FireantUrls.subsidiaries(),
+    });
+  },
+  holders: (): Promise<HoldersResponse> => {
+    return httpFireantService({
+      url: FireantUrls.holders(),
+    });
+  },
+  dividends: (): Promise<DividendsResponse> => {
+    return httpFireantService({
+      url: FireantUrls.dividends(),
+    });
+  },
+  events: (): Promise<EventsResponse> => {
+    return httpFireantService({
+      url: FireantUrls.events(),
+    });
+  },
+  financialIndicators: (): Promise<FinancialIndicatorsResponse> => {
+    return httpFireantService({
+      url: FireantUrls.financialIndicators(),
     });
   },
 };
